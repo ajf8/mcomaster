4ba9919c16de82c1d1e82427096f280785bda10a
Leonardo Rodrigues de Mello <leonardo.rmello@dataprev.gov.br>
2014-07-23 12:41:44 -0300

    mcomaster puppet module should configure initscript defaults
    This commit extend the mcomaster puppet module to configure
    the initscript parameters.
    
    Also this fixes that the mcomaster_port was not working,
    because it was commented on sysconfig/mcomaster template.
    
    example usage:
    class {'mcomaster':
      mcomaster_port         => 8080,
      mcomaster_env          => dev,
      mcomaster_use_thin     => 1,
      mcomaster_thin_use_ssl => 1
    }

--- tools/puppet/modules/mcomaster/manifests/init.pp
+++ tools/puppet/modules/mcomaster/manifests/init.pp
@@ -1,4 +1,4 @@
-class mcomaster ($redis_host='192.168.122.1', $redis_port='6379', $mcomaster_port='3000', $admin_user, $admin_pass, $admin_email) {
+class mcomaster ($redis_host='192.168.122.1', $redis_port=6379, $mcomaster_port=3000, $admin_user, $admin_pass, $admin_email, $mcomaster_env='production', $mcomaster_use_thin=0, $mcomaster_thin_use_ssl=0 ) {
   file { '/etc/sysconfig/mcomaster':
     owner    => 'root',
     group    => 'mcomaster',
