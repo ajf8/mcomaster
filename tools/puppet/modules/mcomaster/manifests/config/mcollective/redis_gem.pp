245860ea69cf8cc8d642bdb35f749f0032cd03ba
Alan F <ajf@eth0.org.uk>
2014-08-31 22:02:53 +0100

    having some problems with latest gem package provider so use exec instead.

--- tools/puppet/modules/mcomaster/manifests/config/mcollective/redis_gem.pp
+++ tools/puppet/modules/mcomaster/manifests/config/mcollective/redis_gem.pp
@@ -1,9 +1,16 @@
 class mcomaster::config::mcollective::redis_gem($version = "latest") {
   if ! defined_with_params(Package['redis'], {'provider' => 'gem' }) { 
-     package{'redis':
-       provider => gem,
-       ensure   => $version,
-       notify   => Service['mcollective']
-     }
+#     package{'redis':
+#       provider => gem,
+#       ensure   => $version,
+#       notify   => Service['mcollective'],
+#       require  => Class['custom_firewall::pre'],
+#     }
+    # some new bug in gem provider perhaps, use exec
+    exec { 'gem_install_redis':
+      command => '/usr/bin/gem install --include-dependencies --no-rdoc --no-ri redis',
+      unless  => '/usr/bin/gem list --local | /bin/egrep -q ^redis',
+      require  => Class['custom_firewall::pre'],
+    }
   }
 }
